cffcdef6522759e7c9b8792b6fec482e9a00f9e4
tian <jiayuetian@foxmail.com>
2024-04-17 23:51:22 +0900

    updated predict.py

--- predict.py
+++ predict.py
@@ -6,13 +6,13 @@ from transformers import BertTokenizerFast
 import dataprocess.rel2text
 from model.model_transformers import UniRelModel
 from model.model_transformers_ner import UniRelModel_ner
-from model.model_transformers_ner_LSTM import UniRelModel_ner_LSTM
+from model.model_transformers_ner import UniRelModel_ner
 from dataprocess.data_extractor import *
 from dataprocess.data_metric import *
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
-UniRelModel = UniRelModel_ner_LSTM
+UniRelModel = UniRelModel_ner
 class UniRel:
     def __init__(self, model_path, max_length=128, dataset_name="nyt") -> None:
         self.model = UniRelModel.from_pretrained(model_path)
@@ -248,7 +248,7 @@ class UniRel:
 
 if __name__ == "__main__":
     # model_path = "/home/tian/Projects/UniRel/model/nyt-checkpoint-final"
-    model_path = "/home/tian/Projects/UniRel/output/nyt-ner-LOC-PER-bsz8/checkpoint-520000"
+    model_path = "/home/tian/Projects/UniRel/output/nyt-NER-LOC-ORG-PER-COUNTRY-t_v_data-bsz24/checkpoint-215000"
     unirel = UniRel(model_path, dataset_name="nyt")
     
     # print(unirel.predict("In perhaps the most ambitious Mekong cruise attempt, Impulse Tourism, an operator based in Chiang Mai, Thailand, is organizing an expedition starting in November in Jinghong, a small city in the Yunnan province in China."))
@@ -256,10 +256,12 @@ if __name__ == "__main__":
 
 
     # print(unirel.predict("Adisham Hall in Sri Lanka was constructed between 1927 and 1931 at St Benedicts Monastery , Adisham , Haputhale , Sri Lanka in the Tudor and Jacobean style of architecture"))
-
+    #
     # t = "I don't think the Yunnan government or any other organization has dominion over the jungles of Xishuangbanna."
     # less sensitive to negative statements.
-    t = "But that spasm of irritation by a master intimidator was minor compared with what Bobby Fischer , the erratic former world chess champion , dished out in March at a news conference in Reykjavik , Iceland ."
+    # t = "But that spasm of irritation by a master intimidator was minor compared with what Bobby Fischer , the erratic former world chess champion , dished out in March at a news conference in Reykjavik , Iceland ."
+    t = 'Paris is in France.'
+    print(unirel.model)
     print(unirel.predict(t))
     # print(unirel._data_process(t))
     # print(unirel.predict([
